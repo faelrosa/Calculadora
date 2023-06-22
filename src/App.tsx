1b9d03c3d9398b8b51781940e9411da15c281a31
Rafael Rosa dos Santos <rafael.santos@alpardobrasil.com.br>
2023-06-22 17:06:34 -0300

    incluindo o projeto

---
+++ src/App.tsx
@@ -0,0 +1,178 @@
+import { useState } from "react";
+import "./App.css";
+
+const taxaPadrao = 1.4;
+const iof = 0.004167;
+const iofDia = 0.38;
+
+function App() {
+  const [pedido, setPedido] = useState(0);
+  const [desconto, setDesconto] = useState(0);
+  const [parcela, setParcela] = useState(0);
+  const pedidoDesconto = pedido - pedido * (desconto / 100);
+  const [calculoParcela, setCalculoParcela] = useState<any[]>([]);
+
+  function calcular() {
+    let pedidoDesconto = pedido - pedido * (desconto / 100);
+    let valorDaParcela = pedidoDesconto / parcela;
+    let calculoIofdia = (iofDia * pedidoDesconto) / 100;
+
+    let dias = 30;
+    let resultadoParcela: any[] = [];
+    for (let index = 0; index < parcela; index++) {
+      let qtdrRealdaparcela = index + 1;
+      let taxa = (taxaPadrao / dias) * (dias * qtdrRealdaparcela);
+      taxa = Number(taxa.toFixed(2));
+      let juros = valorDaParcela * (taxa / 100);
+      juros = Number(juros.toFixed(2));
+      let calculoParcela = valorDaParcela + valorDaParcela * (taxa / 100);
+      let calculoIof =
+        (valorDaParcela * iof * (dias * qtdrRealdaparcela)) / 100;
+      calculoIof = Number(calculoIof.toFixed(2));
+      let valorTotal = calculoParcela + calculoIof;
+      if (Number(qtdrRealdaparcela) === 1) {
+        valorTotal = valorTotal + calculoIofdia;
+      }
+      valorTotal = Number(valorTotal.toFixed(2));
+      resultadoParcela.push({
+        qtdrRealdaparcela,
+        taxa,
+        calculoIof,
+        calculoParcela,
+        parcelasemdesconto: valorDaParcela,
+        valorTotal,
+        calculoIofdia,
+        juros,
+        diasdaParcela: dias * qtdrRealdaparcela,
+      });
+    }
+    console.log(resultadoParcela);
+    setCalculoParcela(resultadoParcela);
+  }
+
+  return (
+    <>
+      <p id="body">
+        <label id="textPedido">Valor do pedido</label>
+        <input
+          min={0}
+          type="number"
+          id="reais"
+          placeholder="R$"
+          onChange={(e) => {
+            setPedido(Number(e.target.value));
+          }}
+        />
+        <p>
+          <label>Parcelas</label>
+          <select
+            id="selecao"
+            onChange={(e) => {
+              setParcela(Number(e.target.value));
+            }}
+          >
+            <option disabled selected value="0">
+              Escolha uma opção
+            </option>
+            <option value="1">1 Parcela</option>
+            <option value="2">2 Parcelas</option>
+            <option value="3">3 Parcelas</option>
+            <option value="4">4 Parcelas</option>
+            <option value="5">5 Parcelas</option>
+          </select>
+        </p>
+        <p>
+          <label>De quanto é o Desconto?</label>
+          <input
+            min={0}
+            type="number"
+            id="desconto"
+            placeholder="% Desconto"
+            onChange={(e) => {
+              setDesconto(Number(e.target.value));
+            }}
+          />
+        </p>
+      </p>
+      <p id="textPDesconto">VALOR COM DESCONTO R${pedidoDesconto}</p>
+      <button onClick={calcular} type="button" id="button">
+        Calcular
+      </button>
+
+      {calculoParcela &&
+        calculoParcela.map((item, indice) => {
+          if (indice === 0) {
+            return (
+              <table className="tabela">
+                <tr>
+                  <th>
+                    Parcela {item.qtdrRealdaparcela}/{parcela}
+                  </th>
+                  <th>Valor da Parcela</th>
+                  <th>{item.diasdaParcela}</th>
+                  <th>{item.taxa.toFixed(2)}%</th>
+                </tr>
+                <tr>
+                  <th>Valor Nominal</th>
+                  <th>R$ {item.parcelasemdesconto}</th>
+                  <th></th>
+                  <th></th>
+                </tr>
+                <tr>
+                  <th>Juros</th>
+                  <th> R$ {item.juros.toFixed(2)}</th>
+                  <th>{item.taxa.toFixed(2)}%</th>
+                  <th></th>
+                </tr>
+                <tr>
+                  <th>IOF</th>
+                  <th> R$ {item.calculoIofdia.toFixed(2)}</th>
+                  <th>% de iof</th>
+                  <th></th>
+                </tr>
+                <tr>
+                  <th>IOF ao dia</th>
+                  <th> R$ {item.calculoIof.toFixed(2)}</th>
+                  <th> % DE Iof ao dia</th>
+                  <th></th>
+                </tr>
+                <tr>
+                  <th>Total</th>
+                  <th> R$ {item.valorTotal}</th>
+                  <th></th>
+                  <th></th>
+                </tr>
+              </table>
+            );
+          } else {
+            return (
+              <table className="tabela">
+                <tr>
+                  <th>
+                    Parcela {item.qtdrRealdaparcela}/{parcela}
+                  </th>
+                  <th>Valor da Parcela</th>
+                  <th>{item.diasdaParcela} Dias</th>
+                  <th>{item.taxa.toFixed(2)}%</th>
+                </tr>
+                <tr>
+                  <th>Valor Nominal</th>
+                  <th>R$ {item.parcelasemdesconto}</th>
+                </tr>
+                <tr>
+                  <th>Juros</th>
+                  <th> R$ {item.juros.toFixed(2)}</th>
+                </tr>
+                <tr>
+                  <th>Total</th>
+                  <th> R$ {item.valorTotal}</th>
+                </tr>
+              </table>
+            );
+          }
+        })}
+    </>
+  );
+}
+
+export default App;
